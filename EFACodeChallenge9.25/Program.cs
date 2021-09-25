1d1a86cc86ae1cb353f3447f8687a2e450a8b9f1
Danny Redden <dannyredden18@gmail.com>
2021-09-25 14:06:59 -0400

    first commit

---
+++ EFACodeChallenge9.25/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFACodeChallenge9._25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] letters = {"s", "u", "p", "e", "r", "c", "a", "l", "i", "f", "r", "a", "g", "i", "l", "i", "s", "t", "i", "c", "e", "x", "p", "i", "a", "l"
+            , "i", "d", "o", "c", "i", "o", "u", "s"};
+            foreach (string letter in letters)
+            {
+                Console.WriteLine(letter);
+            }
+
+            foreach (string letter in letters)
+            {
+                if (letter != "i")
+                {
+                    Console.WriteLine("this is not an I");
+                }
+                else
+                {
+                    Console.WriteLine(letter);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
